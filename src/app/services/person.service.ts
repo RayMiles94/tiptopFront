44fc0371f8858252f1ccb84321cebabf5c20dcb2
Archi4 <tiptoparchi4@gmail.com>
2023-11-17 09:39:53 +0100

    change ip @

--- src/app/services/person.service.ts
+++ src/app/services/person.service.ts
@@ -10,7 +10,7 @@ import { Person } from '../admin-dashboard/person';
   providedIn: 'root'
 })
 export class PersonService {
-  private baseUrl = 'http://localhost:8080/users';
+  private baseUrl = 'http://15.237.223.123:8080/users';
 
   token!:string;
   public loggedUser!:string;
