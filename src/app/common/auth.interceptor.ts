55c8ed5448e41ffc455abc3fb0da2e9c12a1c0aa
RaoufJabriCamptocamp <raouf.jabri@camptocamp.com>
2023-12-09 19:14:50 +0100

    update

--- src/app/common/auth.interceptor.ts
+++ src/app/common/auth.interceptor.ts
@@ -4,18 +4,18 @@ import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
-     headers: any;
-     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        // add authorization header with jwt token if available
-        let jwt = JSON.parse(localStorage.getItem('jwt') || '');
-        if (jwt) {
-            request = request.clone({
-                setHeaders: {
-                    Authorization: `Bearer ${jwt.token}`
-                }
-            });
+  headers: any;
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    // add authorization header with jwt token if available
+    let jwt = localStorage.getItem('jwt');
+    if (jwt) {
+      request = request.clone({
+        setHeaders: {
+          Authorization: `Bearer ${jwt}`
         }
-        return next.handle(request);
+      });
     }
+    return next.handle(request);
+  }
 
 }
